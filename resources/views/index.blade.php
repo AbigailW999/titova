cb8369a14fcc9c4787d034774987a8c2f0abb835
AbigailW <abigailw999@gmail.com>
2019-01-16 11:19:52 +0300

    Admin without CRUD

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,38 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="content__photo1">jhvjh</div>
-    <div class="content__photo2">hvjnk</div>
-    <div class="content__photo3">nbijb</div>
-    <div class="content__photo4">nbijb</div>
-    <div class="content__photo5">nbijb</div>
-    <div class="content__photo6">nbijb</div>
-    <div class="content__photo7">nbijb</div>
-    <div class="content__photo8">nbijb</div>
-    <div class="content__photo9">nbijb</div>
-    <div class="content__photo10">nbijb</div>
-    <div class="content__photo11">nbijb</div>
-    <div class="content__photo12">nbijb</div>
-    <div class="content__photo13">nbijb</div>
-    <div class="content__photo14">nbijb</div>
-    <div class="content__photo15">nbijb</div>
-    <div class="content__photo16">nbijb</div>
+     <!--<div class="content__photo1"> </div>
+    <div class="content__photo2"> </div>
+    <div class="content__photo3"> </div>
+    <div class="content__photo4"> </div>
+    <div class="content__photo5"> </div>
+    <div class="content__photo6"> </div>
+    <div class="content__photo7"> </div>
+    <div class="content__photo8"> </div>
+    <div class="content__photo9"> </div>
+    <div class="content__photo10"> </div>
+    <div class="content__photo11"> </div>
+    <div class="content__photo12"> </div>
+    <div class="content__photo13"> </div>
+    <div class="content__photo14"> </div>
+    <div class="content__photo15"> </div>
+    <div class="content__photo16"> </div> -->
     
-    <!--<img src="{{ asset('img/photo/1-1.jpg') }}">
-    <img src="{{ asset('img/photo/1-2.jpg') }}">
-    <img src="{{ asset('img/photo/3-2.jpg') }}">
-    <img src="{{ asset('img/photo/3-3.jpg') }}">
-    <img src="{{ asset('img/photo/5-1.jpg') }}">
-    <img src="{{ asset('img/photo/5-2.jpg') }}">
-    <img src="{{ asset('img/photo/5-3.jpg') }}">
-    <img src="{{ asset('img/photo/6-3.jpg') }}">
-    <img src="{{ asset('img/photo/7-2.jpg') }}">
-    <img src="{{ asset('img/photo/7-3.jpg') }}">
-    <img src="{{ asset('img/photo/8-1.jpg') }}">
-    <img src="{{ asset('img/photo/8-3.jpg') }}">
-    <img src="{{ asset('img/photo/10-2.jpg') }}">
-    <img src="{{ asset('img/photo/10-3.jpg') }}">
-    <img src="{{ asset('img/photo/10-1.jpg') }}">
-    <img src="{{ asset('img/photo/9-3.jpg') }}"> -->
+   <img src="{{ asset('img/photo/1-1.jpg') }}" style="width:30%">
+    <img src="{{ asset('img/photo/1-2.jpg') }}" style="width:30%">
+    <img src="{{ asset('img/photo/3-2.jpg') }}" style="width:30%">
+    <img src="{{ asset('img/photo/3-3.jpg') }}" style="width:30%">
+    <img src="{{ asset('img/photo/5-1.jpg') }}" style="width:30%">
+    <img src="{{ asset('img/photo/5-2.jpg') }}" style="width:30%">
+    <img src="{{ asset('img/photo/5-3.jpg') }}" style="width:45%">
+    <img src="{{ asset('img/photo/6-3.jpg') }}" style="width:45%">
+    <img src="{{ asset('img/photo/7-2.jpg') }}" style="width:30%">
+    <img src="{{ asset('img/photo/7-3.jpg') }}" style="width:30%">
+    <img src="{{ asset('img/photo/8-1.jpg') }}" style="width:30%">
+    <img src="{{ asset('img/photo/8-3.jpg') }}" style="width:30%">
+    <img src="{{ asset('img/photo/10-2.jpg') }}" style="width:30%">
+    <img src="{{ asset('img/photo/10-3.jpg') }}" style="width:30%">
+    <img src="{{ asset('img/photo/10-1.jpg') }}" style="width:45%">
+    <img src="{{ asset('img/photo/9-3.jpg') }}"  style="width:45%">
 
 @endsection
